c13eb51db992a6d6021484e0ec93a4629cca1f0b
Liza Khaletskaya <khaletskayali@gmail.com>
2024-12-01 18:21:44 +0300

    feat:solve 4 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -614,8 +614,8 @@ function getSumOfNumbers(x1, x2, x3) {
  * -5, -6 => -5
  * 0, 5   => 5
  */
-function getMaxNumber(/* firstNumber, secondNumber */) {
-  throw new Error('Not implemented');
+function getMaxNumber(firstNumber, secondNumber) {
+  return Math.max(firstNumber, secondNumber);
 }
 
 /**
@@ -630,8 +630,8 @@ function getMaxNumber(/* firstNumber, secondNumber */) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 /**
@@ -644,8 +644,8 @@ function getRandomInteger(/* min, max */) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -661,8 +661,29 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  const arr = [];
+  let arrNum = 0;
+  if (number > 0) {
+    while (arrNum <= number) {
+      arr.push(arrNum);
+      arrNum += 1;
+    }
+  } else {
+    while (arrNum >= number) {
+      arr.push(arrNum);
+      arrNum -= 1;
+    }
+  }
+
+  let count = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] % 2 !== 0) {
+      count += 1;
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
